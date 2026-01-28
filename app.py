33f5f21ee6b9ff7d95be9a0edc5f8cd3593459ba
Piyush mehta <110559166+piyush9784@users.noreply.github.com>
2026-01-29 04:46:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+# MongoDB connection
+MONGO_URI = os.getenv(
+    MONGO_URI = "mongodb+srv://piyushmehta200_db_user:<db_password>@cluster0.gg2yymv.mongodb.net/?appName=Cluster0"
+    
+)
+
+client = MongoClient(MONGO_URI)
+db = client["github_events"]
+collection = db["events"]
+
+
+@app.route("/webhook", methods=["POST"])
 def github_webhook():
-    event_type = request.headers.get('X-GitHub-Event')
+    event_type = request.headers.get("X-GitHub-Event")
     payload = request.json
 
-    print("Received GitHub Event:", event_type)
-    print("Payload:", payload)
+    if event_type == "push":
+        data = {
+            "author": payload.get("author", "unknown"),
+            "action": "PUSH",
+            "from_branch": payload.get("from_branch", ""),
+            "to_branch": payload.get("to_branch", ""),
+            "timestamp": datetime.utcnow().isoformat()
+        }
+
+        collection.insert_one(data)
+        print("Saved to MongoDB:", data)
 
     return jsonify({"status": "received"}), 200
 
